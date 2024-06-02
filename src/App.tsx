15b8f484701a244cdf0eecbba710832b5d32953b
Shin-sibainu <shincode0712@gmail.com>
2024-06-02 14:58:33 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import TodoApp from "./components/TodoApp";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <TodoApp />
-    </>
+    </QueryClientProvider>
   );
 }
 
